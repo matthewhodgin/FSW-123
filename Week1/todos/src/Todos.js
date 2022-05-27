a8b389443d200726a04e2fefbd55fcf7b0776e8a
matthewhodgin <matthew.a.hodgin@bryanuniversity.edu>
2022-05-27 08:07:56 -0700

    Week1

---
+++ Week1/todos/src/Todos.js
@@ -0,0 +1,15 @@
+function Todos () {
+
+    return (
+        <>
+        <input>go to gym</input>
+        <input>do laundry</input>
+        <input>food shopping</input> 
+        <input>do homework</input>
+        </>
+    );
+}
+
+export default Todos;
+
+
