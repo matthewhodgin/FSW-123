e7b03af5cd1a733148541d34362316a73a877fff
matthewhodgin <matthew.a.hodgin@bryanuniversity.edu>
2022-05-29 18:27:42 -0700

    Week2

--- Week2/Card.js
+++ Week2/Card.js
@@ -7,19 +7,33 @@ function Card(props) {
         </div>
     );
 }
+export const Box = [
+    {
+        title: 1,
+        subtitle: 1,
+        desc: "Hello World 1!",
+        bgColor: "red",
+    },
+    {
+        title: 2,
+        subtitle: 2,
+        desc: "Hello World 2!",
+        bgColor: "orange",
+    },
+    {
+        title: 3,
+        subtitle: 3,
+        desc: "Hello World 3!",
+        bgColor: "yellow",
+    },
+    {
+        title: 4,
+        subtitle: 4,
+        desc: "Hello World 4!",
+        bgColor: "green"
+    }
 
-
-// function Box({key, src, alt, desc, location})) {
-
-//     return (
-//         <div key = {key}>
-//                 <img src = {src} alt = {alt} />
-//                 <h4>{desc}</h4>
-//                 <h5> I live in {location}</h5>
-
-//         </div>
-//     );
-// }
+];
 
 export default Card;
 
