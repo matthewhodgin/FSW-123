d3326e2144c8fe932e9971cd2bc970e142a7be51
matthewhodgin <matthew.a.hodgin@bryanuniversity.edu>
2022-06-12 12:39:40 -0700

    Week3

---
+++ Week3/diceapp/src/Die.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import './Dice.css'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import{ faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix } from '@fortawesome/free-solid-svg-icons'
+
+class Die extends Component{
+render(){
+    const {face, rolling} = this.props
+      
+    // Using font awesome icon to show
+    // the exactnumber of dots
+    return (
+            <div>
+                <FontAwesomeIcon icon={ face == "one"?faDiceOne: face == "two"?faDiceTwo: face == "three"?faDiceThree: face == "four"?faDiceFour: face == "five"?faDiceFive: }  className={`Die 
+                ${rolling && 'Die-shaking'}`} />
+            </div >
+        )
+}
+}
+  
+export default Die
